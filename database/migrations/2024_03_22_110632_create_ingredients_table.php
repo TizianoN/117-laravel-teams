0ef41011f978e2c6215fde9555b70a9ecf92182f
SimoneCappadona <simocappa95@gmail.com>
2024-03-22 12:17:28 +0100

    fix

--- database/migrations/2024_03_22_110632_create_ingredients_table.php
+++ database/migrations/2024_03_22_110632_create_ingredients_table.php
@@ -15,8 +15,9 @@ return new class extends Migration
     {
         Schema::create('ingredients', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('origin');
+            // Name for ingredient
+            $table->string('name', 50);
+            $table->string('origin', 100);
             $table->timestamps();
         });
     }
